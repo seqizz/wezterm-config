f803e907a47a7f4278ab1ffdd6a485eb42fd7b29
Gürkan <gitoperations@gurkan.in>
2024-02-02 15:34:02 +0100

    Better window titles

--- window.lua
+++ window.lua
@@ -1,15 +1,29 @@
-local padding = 0
-local window_padding = {
-  left = padding,
-  right = padding,
-  top = padding,
-  bottom = padding,
-}
+local wezterm = require('wezterm')
+local padding = 4
 
-local M = {
-  window_decorations = "NONE",
-  window_close_confirmation = "NeverPrompt",
-  window_padding = window_padding,
+wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
+  local zoomed = ''
+
+  local active_pane = wezterm.mux.get_pane(tab.active_pane.pane_id)
+  local domain_name = active_pane:get_domain_name()
+
+  local index = ''
+  if #tabs > 1 or domain_name == 'default' then
+    index = string.format('[%s][%d] ', active_pane:get_domain_name() , #tabs)
+  end
+
+  return index .. tab.active_pane.title
+end)
+
+return {
+  window_decorations = 'NONE',
+  window_close_confirmation = 'NeverPrompt',
+  window_padding = {
+    left = padding,
+    right = padding,
+    top = padding,
+    bottom = padding,
+  },
   window_background_image_hsb = {
     brightness = 0.3,
   },
@@ -17,5 +31,3 @@ local M = {
   text_background_opacity = 1.0,
   adjust_window_size_when_changing_font_size = false,
 }
-
-return M
