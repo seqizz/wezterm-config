9943e60f64440a105865ca31ef7390a9ecb0a169
Gürkan <gitoperations@gurkan.in>
2025-05-26 10:24:03 +0200

    Switch to my man

--- tmux-switch.lua
+++ tmux-switch.lua
@@ -36,17 +36,18 @@ return {
           { source = { Color = '#282828' }, opacity = 0.95, width = '100%', height = '100%' },
           {
             source = {
-              File = wezterm.home_dir .. '/.config/wezterm/assets/plane.png',
+              File = wezterm.home_dir .. '/.config/wezterm/assets/my-man.png',
             },
-            width = '3cell',
-            height = '15cell',
+            width = '12cell',
+            height = '6cell',
             repeat_x = 'NoRepeat',
             repeat_y = 'NoRepeat',
             hsb = dimmer,
-            opacity = 0.4,
+            opacity = 0.03,
             horizontal_align = 'Right',
             vertical_align = 'Bottom',
-            horizontal_offset = -30,
+            vertical_offset = -20,
+            -- horizontal_offset = -30,
           },
         },
       })
