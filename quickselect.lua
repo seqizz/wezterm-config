75458fcf8466bcef698632cb31147ad2a9de8aea
Gürkan <gitoperations@gurkan.in>
2026-05-18 19:40:13 +0200

    Fix quickselect even more

--- quickselect.lua
+++ quickselect.lua
@@ -6,13 +6,12 @@ return {
     'AQ\\S{38}', -- CephX secret
     '[0-9a-f]{7,40}', -- sha1
     'sha256:[A-Za-z0-9]{52}', -- sha256
-    '%d+%.%d+%.%d+%.%d+', -- IPv4
     '[A-Fa-f0-9:]+::?[A-Fa-f0-9:]*', -- IPv6
-    '[a-z0-9-]+.[a-z0-9]+.[a-z0-9]+.[a-z0-9]+ ', -- inno FQDN
+    '[a-z0-9-]+\\.[a-z0-9]+\\.[a-z0-9]+\\.[a-z0-9]+', -- inno FQDN and ipv4
     '[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9.-]+', -- simple e-mail
     '\'(\\S[^\']*\\S)\'', -- single quoted text
     '"(\\S[^"]*\\S)"', -- double quoted text
-    '~?/?[a-zA-Z0-9_/.-]+', -- path
+    '~?/[a-zA-Z0-9_/.-]+|~[a-zA-Z0-9_/.-]*', -- path (require / or ~)
     '(?<=\\)\\s)[^\\s]+', -- git branch name on my prompt
     '(?<=~>\\s).*?(?=\\s*(?:\\(|\\[))', -- stuff after ~> because that is the prompt
     -- '%s(.*)%s', -- anything else covered with whitespace
